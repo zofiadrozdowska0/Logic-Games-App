1983ca034326d550fb3d94020dfe0c7e47aab760
Łukasz Marcinkowski <s188954@student.pg.edu.pl>
2024-06-01 13:14:04 +0000

    Update MemoryBoard.kt

--- Zapamiętywanie elementów zbioru/app/src/main/java/com/example/remembersequence/MemoryBoard.kt
+++ Zapamiętywanie elementów zbioru/app/src/main/java/com/example/remembersequence/MemoryBoard.kt
@@ -11,17 +11,14 @@ import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.remembersequence.models.BoardSize
 import kotlin.math.min
-import android.graphics.Color
 import android.util.Log
 import android.widget.Toast
 
 class MemoryBoard(
     private val context: Context,
     private val boardSize: BoardSize,
-    private val cardImages: List<Int>,
-    private val rvBoard: RecyclerView
-) :
-    RecyclerView.Adapter<MemoryBoard.ViewHolder>() {
+    private val cardImages: List<Int>
+) : RecyclerView.Adapter<MemoryBoard.ViewHolder>() {
 
     companion object {
         private const val TAG = "MemoryBoard"
@@ -29,9 +26,9 @@ class MemoryBoard(
         private const val DELAY_BEFORE_HIDING_UPPER_CARDS = 5000L // 5 seconds
     }
 
-    private val revealedCards = MutableList(boardSize.numCards) { false }
     private var isClickable = false
-    private var sequence = generateRandomSequence()
+    private var sequence: List<Int> = generateRandomSequence()
+    private var secondHalfImages: Pair<List<Int>, Int> = generateRandomImagesForSecondHalf(sequence)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val cardWidth = parent.width / 2 - 2 * MARGIN_SIZE
@@ -52,116 +49,70 @@ class MemoryBoard(
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
-        var pozycja: Int = -1
-        var tlo: List<Int> = emptyList()
-        var clickedPosition = -1
-
-        init {
-            updateImagesForSecondHalf()
-        }
-
-        private fun updateImagesForSecondHalf() {
-            val (tloList, nonMatchingPosition) = generateRandomImagesForSecondHalf()
-            pozycja = nonMatchingPosition
-            tlo = tloList
-        }
+        private val imageButton: ImageButton = itemView.findViewById(R.id.imageButton)
 
         fun bind(position: Int) {
-            clickedPosition = position
-
-            if (sequence != null) {
-                if (position < sequence.size) {
-                    // Ustawienie obrazków na pierwszej połowie kart
-                    imageButton.setImageResource(sequence[position])
-                } else {
-                    // Ustawienie obrazków na drugiej połowie kart
-                    val secondHalfPosition = position - sequence.size
-                    imageButton.setImageResource(R.drawable.ic_launcher_background)
-                }
-
-                // Opóźnienie zmiany obrazka karty po 5 sekundach
-                Handler(Looper.getMainLooper()).postDelayed({
-                    isClickable = true
-                    if (clickedPosition >= sequence!!.size) {
-                        // Ustawienie obrazków na drugiej połowie kart takich samych jak na pierwszej połowie
-                        val secondHalfPosition = clickedPosition - sequence!!.size
-                        imageButton.setImageResource(tlo[secondHalfPosition])
-                    } else {
-                        // Ustawienie obrazka na tło po 5 sekundach na pierwszej połowie kart
-                        imageButton.setImageResource(R.drawable.ic_launcher_background)
-                    }
-                }, DELAY_BEFORE_HIDING_UPPER_CARDS)
+            // Display the initial sequence of images
+            if (position < sequence.size) {
+                imageButton.setImageResource(sequence[position])
             } else {
                 imageButton.setImageResource(R.drawable.ic_launcher_background)
             }
 
+            // Hide images after delay
+            Handler(Looper.getMainLooper()).postDelayed({
+                isClickable = true
+                if (position >= sequence.size) {
+                    val secondHalfPosition = position - sequence.size
+                    imageButton.setImageResource(secondHalfImages.first[secondHalfPosition])
+                } else {
+                    imageButton.setImageResource(R.drawable.ic_launcher_background)
+                }
+            }, DELAY_BEFORE_HIDING_UPPER_CARDS)
+
+            // Set click listener for second half images
             imageButton.setOnClickListener {
-                if (isClickable) {
-                    if (clickedPosition >= sequence.size && clickedPosition < boardSize.numCards) {
-                        // Kliknięcie na kartę w drugiej połowie (odpowiedź)
-                        val a = pozycja
-                        Log.d(TAG, "Clicked position: $clickedPosition")
-                        Log.d(TAG,"$a")
-                        if (clickedPosition - sequence.size == a) {
-                            // Kliknięta karta jest na pozycji nonMatchingPosition
-                            showToast("Masz punkt!")
-                            restartGame()
-                        } else {
-                            showToast("Błąd!")
-                            restartGame()
-                        }
+                if (isClickable && position >= sequence.size) {
+                    val secondHalfPosition = position - sequence.size
+                    val clickedImage = secondHalfImages.first[secondHalfPosition]
+                    val correctImage = secondHalfImages.first[secondHalfImages.second]
+
+                    if (clickedImage == correctImage) {
+                        showToast("Masz punkt!")
                     } else {
-                        // Kliknięcie na kartę w pierwszej połowie
-                        // Tutaj możesz dodać logikę, jeśli chcesz obsłużyć kliknięcie na karty w pierwszej połowie
+                        showToast("Błąd!")
                     }
-                    // Logowanie pozycji
+                    restartGame()
                 }
             }
         }
     }
 
-
     private fun showToast(message: String) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
+
     private fun generateRandomSequence(): List<Int> {
         return cardImages.shuffled().distinct().take(4)
     }
 
-    private fun generateRandomImagesForSecondHalf(): Pair<List<Int>, Int> {
-        val sequenceElements = sequence ?: return Pair(emptyList(), -1)
-        val nonSequenceImages = cardImages.distinct().filter { it !in sequenceElements }
-
-        val randomSequenceImages = mutableListOf<Int>()
-        while (randomSequenceImages.size < 3) {
-            val randomElement = sequenceElements.shuffled().firstOrNull()
-            if (randomElement != null && randomElement !in randomSequenceImages) {
-                randomSequenceImages.add(randomElement)
-            }
+    private fun generateRandomImagesForSecondHalf(sequence: List<Int>): Pair<List<Int>, Int> {
+        var secondHalfImages = sequence.shuffled().toMutableList()
+        val nonSequenceImages = cardImages.filter { it !in sequence }
+        if (nonSequenceImages.isNotEmpty()) {
+            val randomNonSequenceImage = nonSequenceImages.random()
+            val randomIndex = (0 until secondHalfImages.size).random() // Losowy indeks od 0 do wielkości listy
+            secondHalfImages[randomIndex] = randomNonSequenceImage
+            return Pair(secondHalfImages, randomIndex)
+        } else {
+            Log.e(TAG, "Brak dostępnych obrazków spoza sekwencji")
+            return Pair(secondHalfImages.shuffled(), secondHalfImages.lastIndex)
         }
-
-        // Losujemy jeden obraz spoza sekwencji
-        val randomNonSequenceImage = nonSequenceImages.randomOrNull()
-
-        // Zapisujemy pozycję niepasującego elementu
-        val nonMatchingPosition = randomSequenceImages.size
-
-        // Tworzymy listę obrazków na drugiej połowie karty
-        val secondHalfImages = mutableListOf<Int>()
-        secondHalfImages.addAll(randomSequenceImages)
-        randomNonSequenceImage?.let { secondHalfImages.add(it) }
-
-        // Tasujemy kolejność kart na dole
-        return Pair(secondHalfImages.shuffled(), nonMatchingPosition)
     }
 
     private fun restartGame() {
         sequence = generateRandomSequence()
-
+        secondHalfImages = generateRandomImagesForSecondHalf(sequence)
         notifyDataSetChanged()
     }
 }
-
-
-
