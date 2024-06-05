8766ff1f4999b30211ab7b86ce9e03284f160db4
Maksymilian Burdziej <maks@burdziej.pl>
2024-06-05 02:05:09 +0200

    Sledzenie aktywnosci znajomych, usuwanie znajomych, usuwanie zaproszen

--- logowanie_kalendarz_sidebar/app/src/main/java/com/example/signupapp/checkfriend.kt
+++ logowanie_kalendarz_sidebar/app/src/main/java/com/example/signupapp/checkfriend.kt
@@ -1,70 +1,192 @@
-//package com.example.signupapp
-//
-//import android.content.Intent
-//import android.os.Bundle
-//import android.widget.LinearLayout
-//import com.google.android.material.floatingactionbutton.FloatingActionButton
-//
-//import android.widget.Button
-//import android.widget.TextView
-//import androidx.appcompat.app.ActionBarDrawerToggle
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.appcompat.widget.Toolbar
-//import androidx.core.view.GravityCompat
-//import androidx.drawerlayout.widget.DrawerLayout
-//import com.google.android.material.navigation.NavigationView
-//
-//class checkfriend : AppCompatActivity() {
-//    private lateinit var drawerLayout: DrawerLayout
-//
-//    private lateinit var toggle: ActionBarDrawerToggle
-//    private lateinit var dbHelper: DBHelper
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_friendcheck)
-//        dbHelper = DBHelper(this) // Zainicjuj dbHelper
-//        val toolbar: Toolbar = findViewById(R.id.toolbar)
-//        val toolbarTitle = dbHelper.getUsernameById(MainActivity.CurrentUser.userId)
-//        toolbar.title = toolbarTitle
-//        setSupportActionBar(toolbar)
-//
-//        drawerLayout = findViewById(R.id.drawer_layout)
-//        val navigationView: NavigationView = findViewById(R.id.nav_view)
-//
-//        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
-//        drawerLayout.addDrawerListener(toggle)
-//        toggle.syncState()
-//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-//        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_ryba_navbar)  // Ikona menu hamburger (dodaj plik do drawable)
-//
-//        navigationView.setNavigationItemSelectedListener { menuItem ->
-//            when (menuItem.itemId) {
-//                R.id.nav_home -> {
-//                    val intent = Intent(applicationContext, succes::class.java)
-//                    startActivity(intent)
-//                }
-//                R.id.nav_rules -> {
-//                    val intent = Intent(applicationContext, rules::class.java)
-//                    startActivity(intent)
-//                }
-//                R.id.nav_friends -> {
-//                    val intent = Intent(applicationContext, friends::class.java)
-//                    startActivity(intent)
-//                }
-//                R.id.nav_logout -> {
-//                    val intent = Intent(applicationContext, MainActivity::class.java)
-//                    startActivity(intent)
-//                }
-//            }
-//            drawerLayout.closeDrawer(GravityCompat.START)
-//            true
-//        }
-//    }
-//
-//    override fun onOptionsItemSelected(item: android.view.MenuItem): Boolean {
-//        if (toggle.onOptionsItemSelected(item)) {
-//            return true
-//        }
-//        return super.onOptionsItemSelected(item)
-//    }
-//}
+package com.example.signupapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import java.text.SimpleDateFormat
+import java.util.*
+
+class checkfriend : AppCompatActivity() {
+
+    private var dataPointsList: List<List<Pair<Float, Float>>> = emptyList()
+    private var username: String? = null  // To hold the friend's username
+    private lateinit var currentUserId: String
+    val firestore = FirebaseFirestore.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friendcheck)
+
+        currentUserId = FirebaseAuth.getInstance().currentUser?.uid ?: ""
+        username = intent.getStringExtra("FRIEND_USERNAME")
+        if (username.isNullOrEmpty()) {
+            Toast.makeText(this, "No username provided", Toast.LENGTH_SHORT).show()
+            finish()  // Close the activity if no username
+        } else {
+            val textView = findViewById<TextView>(R.id.textView10)
+            textView.text = "$username"  // Display the username
+            fetchDataPointsFromDatabase()
+        }
+
+        val removeFriendButton = findViewById<Button>(R.id.remove_friend_Button)
+        removeFriendButton.setOnClickListener {
+            findUserIdByUsername(username.toString()) { userId ->
+                if (userId != null) {
+                    removeFriend(userId)
+                } else {
+                    Toast.makeText(this, "No user found with that username.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun removeFriend(friend_id: String) {
+
+        // Usuń rekordy gdzie currentUserId to Invitee, a friend_id to Inviter
+        firestore.collection("friends")
+            .whereEqualTo("Invitee", currentUserId)
+            .whereEqualTo("Inviter", friend_id)
+            .get()
+            .addOnSuccessListener { documents ->
+                removeDocuments(documents)
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "Error fetching original documents: ${e.message}", Toast.LENGTH_LONG).show()
+            }
+
+        // Usuń rekordy gdzie friend_id to Invitee, a currentUserId to Inviter
+        firestore.collection("friends")
+            .whereEqualTo("Invitee", friend_id)
+            .whereEqualTo("Inviter", currentUserId)
+            .get()
+            .addOnSuccessListener { documents ->
+                removeDocuments(documents)
+                Toast.makeText(this, "Usunieto znajomego", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "Error fetching reversed documents: ${e.message}", Toast.LENGTH_LONG).show()
+            }
+
+        Thread.sleep(300)
+        navigateToFriendsActivity() // Navigate to the friends view after handling the invitation
+    }
+
+    private fun removeDocuments(documents: QuerySnapshot) {
+        if (documents.isEmpty) {
+            return
+        }
+        for (document in documents) {
+            firestore.collection("friends").document(document.id)
+                .delete()
+                .addOnSuccessListener {
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(this, "Error removing invitation: ${e.message}", Toast.LENGTH_LONG).show()
+                }
+        }
+    }
+
+
+    private fun fetchDataPointsFromDatabase() {
+        val db = FirebaseFirestore.getInstance()
+        db.collection("points")
+            .whereEqualTo("username", username)
+            .get()
+            .addOnSuccessListener { result ->
+                val pointsMap = processResult(result)
+                val dataPoints = mapToPointsList(pointsMap)
+                setDataPointsList(dataPoints)
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(this, "Error getting documents: $exception", Toast.LENGTH_LONG).show()
+            }
+    }
+
+    private fun processResult(result: QuerySnapshot): Map<String, Pair<Date, Float>> {
+        val pointsMap = mutableMapOf<String, Pair<Date, Float>>()
+        for (document in result) {
+            val date = document.getTimestamp("date")?.toDate()
+            val reflexPointsValue = document.getLong("reflex_points")?.toFloat() ?: 0f
+
+            date?.let {
+                val formattedDate = formatDate(it)
+                updatePointsMap(pointsMap, formattedDate, it, reflexPointsValue)
+            }
+        }
+        return pointsMap
+    }
+
+    private fun updatePointsMap(pointsMap: MutableMap<String, Pair<Date, Float>>, formattedDate: String, dateObj: Date, pointsValue: Float) {
+        val currentEntry = pointsMap[formattedDate]
+        if (currentEntry == null || dateObj.after(currentEntry.first)) {
+            pointsMap[formattedDate] = Pair(dateObj, pointsValue)
+        }
+    }
+
+    private fun mapToPointsList(pointsMap: Map<String, Pair<Date, Float>>): List<List<Pair<Float, Float>>> {
+        val list = mutableListOf<Pair<Float, Float>>()
+        pointsMap.forEach { (_, pair) ->
+            list.add(Pair(getDayIndex(formatDate(pair.first)).toFloat(), pair.second))
+        }
+        return listOf(list)
+    }
+
+    private fun setDataPointsList(dataPointsList: List<List<Pair<Float, Float>>>) {
+        this.dataPointsList = dataPointsList
+        // Here you would update your view with the new data points
+    }
+
+    private fun formatDate(date: Date): String {
+        val dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+        return dateFormat.format(date)
+    }
+
+    private fun getDayIndex(formattedDate: String): Int {
+        val dates = getDatesFromLast7Days()
+        return dates.indexOf(formattedDate)
+    }
+
+    private fun getDatesFromLast7Days(): List<String> {
+        val dates = mutableListOf<String>()
+        val calendar = Calendar.getInstance()
+        for (i in 0..6) {
+            dates.add(SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(calendar.time))
+            calendar.add(Calendar.DATE, -1)
+        }
+        return dates
+    }
+
+    fun findUserIdByUsername(username: String, callback: (String?) -> Unit) {
+        val db = FirebaseFirestore.getInstance()
+        db.collection("users")
+            .whereEqualTo("username", username)
+            .limit(1)  // Zakładamy, że nazwy użytkowników są unikalne
+            .get()
+            .addOnSuccessListener { documents ->
+                if (documents.isEmpty) {
+                    callback(null)  // Brak użytkownika o tej nazwie
+                    return@addOnSuccessListener
+                }
+                for (document in documents) {
+                    val userId = document.id  // Pobierz ID użytkownika
+                    callback(userId)
+                    return@addOnSuccessListener
+                }
+            }
+            .addOnFailureListener { exception ->
+                println("Error getting documents: $exception")
+                callback(null)  // Obsługa błędu, zwróć null
+            }
+    }
+    private fun navigateToFriendsActivity() {
+        val intent = Intent(applicationContext, friends::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
--- logowanie_kalendarz_sidebar/app/src/main/java/com/example/signupapp/friends.kt
+++ logowanie_kalendarz_sidebar/app/src/main/java/com/example/signupapp/friends.kt
@@ -6,6 +6,7 @@ import android.view.MenuItem
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -15,6 +16,7 @@ import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 
 class friends : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
@@ -65,8 +67,8 @@ class friends : AppCompatActivity() {
                     startActivity(intent)
                 }
                 R.id.nav_friends -> {
-                    // Nie trzeba ponownie uruchamiać tej samej aktywności
-                    drawerLayout.closeDrawer(GravityCompat.START)
+                    val intent = Intent(applicationContext, friends::class.java)
+                    startActivity(intent)
                 }
                 R.id.nav_logout -> {
                     FirebaseAuth.getInstance().signOut()
@@ -149,7 +151,7 @@ class friends : AppCompatActivity() {
                         textView.setPadding(18, 18, 16, 16)
                         linearLayout.addView(textView)
                         textView.setOnClickListener {
-                            // Możesz dodać obsługę kliknięcia tutaj
+                            removeFriend(userID)
                         }
                     }
                 }
@@ -173,15 +175,68 @@ class friends : AppCompatActivity() {
                         textView.setPadding(18, 18, 16, 16)
                         linearLayout.addView(textView)
                         textView.setOnClickListener {
-                            //val intent = Intent(applicationContext, checkfriend::class.java)
-                            //startActivity(intent)
+                            val intent = Intent(this, checkfriend::class.java)
+                            intent.putExtra("FRIEND_USERNAME", userName)  // Pass the friend's username
+                            startActivity(intent)
                         }
+
                     }
                 }
                 .addOnFailureListener { exception ->
                     // Obsługa błędu
                 }
         }
+    }
+
+    private fun removeFriend(friend_id: String) {
+
+        // Usuń rekordy gdzie currentUserId to Invitee, a friend_id to Inviter
+        firestore.collection("friends")
+            .whereEqualTo("Invitee",currentUserID)
+            .whereEqualTo("Inviter", friend_id)
+            .get()
+            .addOnSuccessListener { documents ->
+                removeDocuments(documents)
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "Error fetching original documents: ${e.message}", Toast.LENGTH_LONG).show()
+            }
+
+        // Usuń rekordy gdzie friend_id to Invitee, a currentUserId to Inviter
+        firestore.collection("friends")
+            .whereEqualTo("Invitee", friend_id)
+            .whereEqualTo("Inviter", currentUserID)
+            .get()
+            .addOnSuccessListener { documents ->
+                removeDocuments(documents)
+                Toast.makeText(this, "Usunieto znajomego", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "Error fetching reversed documents: ${e.message}", Toast.LENGTH_LONG).show()
+            }
+        Thread.sleep(300)
+        navigateToFriendsActivity() // Navigate to the friends view after handling the invitation
+    }
+
+    private fun removeDocuments(documents: QuerySnapshot) {
+        if (documents.isEmpty) {
+            return
+        }
+        for (document in documents) {
+            firestore.collection("friends").document(document.id)
+                .delete()
+                .addOnSuccessListener {
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(this, "Error removing invitation: ${e.message}", Toast.LENGTH_LONG).show()
+                }
+        }
+    }
+
+    private fun navigateToFriendsActivity() {
+        val intent = Intent(applicationContext, friends::class.java)
+        startActivity(intent)
+        finish()
     }
 }
 
